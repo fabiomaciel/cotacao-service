88d40ad45674d99b511d22a57529e780a608e564
fabio maciel <6810827+fabiomaciel@users.noreply.github.com>
2022-07-18 11:55:41 -0300

    feat: add springboot boilerplate

---
+++ src/test/kotlin/io/github/fabiomaciel/cotacaoservice/CotacaoServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.fabiomaciel.cotacaoservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CotacaoServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
